e90a42eaabffc87567e5f450d098d624e5b3ba2c
Yash Garg <yashygkf@gmail.com>
2021-09-13 14:17:33 +0530

    scrolling button correction

--- lib/widgets/sub_chp_screen/sub_chp_main_view.dart
+++ lib/widgets/sub_chp_screen/sub_chp_main_view.dart
@@ -1,22 +1,28 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
+import 'package:flutter/scheduler.dart';
 
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import "package:font_awesome_flutter/font_awesome_flutter.dart";
 
+/// Root Widget of the Subject's Chapter Screen Main Component.
 class SubChpMainView extends StatelessWidget {
+  ///
   const SubChpMainView({
     Key? key,
     required this.height,
     required this.width,
   }) : super(key: key);
 
+  /// Height of the Screen in which this widget is used.
   final double height;
+
+  /// Width of the Screen in which this widget is used.
   final double width;
 
   @override
   Widget build(BuildContext context) {
+    final _scrollController = ScrollController();
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
       children: <Widget>[
         Container(
           height: height * 0.07,
@@ -24,7 +30,7 @@ class SubChpMainView extends StatelessWidget {
           child: Container(
             alignment: Alignment.topLeft,
             child: Text(
-              'Subject Code',
+              "Subject Code",
               style: Theme.of(context).textTheme.headline6,
             ),
           ),
@@ -34,8 +40,6 @@ class SubChpMainView extends StatelessWidget {
           decoration: BoxDecoration(
             color: Theme.of(context).backgroundColor,
             borderRadius: const BorderRadius.only(
-              bottomLeft: Radius.zero,
-              bottomRight: Radius.zero,
               topLeft: Radius.circular(10),
               topRight: Radius.circular(10),
             ),
@@ -47,29 +51,29 @@ class SubChpMainView extends StatelessWidget {
             //color: Colors.cyan,
             height: height * 0.44,
             child: ListView.builder(
+              controller: _scrollController,
               itemBuilder: (BuildContext ctx, int i) => Material(
                 elevation: 2,
                 color: Colors.transparent,
                 shadowColor: Colors.transparent,
                 child: InkWell(
-                  onTap: (){},
+                  onTap: () {},
                   child: Container(
                     margin: EdgeInsets.symmetric(
                       vertical: height * 0.007,
                     ),
                     height: height * 0.1,
                     decoration: BoxDecoration(
-                      color: Theme.of(context).buttonTheme.colorScheme!.background,
+                      color: Colors.white,
                       borderRadius: BorderRadius.circular(10),
                     ),
-                    padding: EdgeInsets.all(
-                      width * 0.07,
-                    ),
-                    child: Text(
-                      'Chapter',
-                      textAlign: TextAlign.center,
-                      style: Theme.of(context).textTheme.button,
-                      overflow: TextOverflow.ellipsis,
+                    child: Center(
+                      child: Text(
+                        "Chapter",
+                        textAlign: TextAlign.center,
+                        style: Theme.of(context).textTheme.button,
+                        overflow: TextOverflow.ellipsis,
+                      ),
                     ),
                   ),
                 ),
@@ -80,15 +84,25 @@ class SubChpMainView extends StatelessWidget {
         ),
         Container(
           height: height * 0.05,
-          decoration: BoxDecoration(
+          decoration: const BoxDecoration(
             color: Colors.brown,
           ),
           width: width,
           child: IconButton(
-            icon: FaIcon(FontAwesomeIcons.chevronDown),
-            onPressed: (){},
+            icon: const FaIcon(FontAwesomeIcons.chevronDown),
+            onPressed: () async {
+              // Delay to make sure the frames are rendered properly
+              await Future.delayed(const Duration(milliseconds: 100));
+              SchedulerBinding.instance?.addPostFrameCallback((_) {
+                _scrollController.animateTo(
+                  _scrollController.position.viewportDimension,
+                  duration: const Duration(milliseconds: 900),
+                  curve: Curves.fastOutSlowIn,
+                );
+              });
+            },
           ),
-        )
+        ),
       ],
     );
   }
