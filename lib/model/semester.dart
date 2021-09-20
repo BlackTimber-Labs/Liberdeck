c674aa68561071899cbd945a451238f04dd2d7e9
Yash Garg <yashygkf@gmail.com>
2021-09-21 03:03:16 +0530

    semester sorting complete

--- lib/model/semester.dart
+++ lib/model/semester.dart
@@ -2,14 +2,14 @@
 class Semester {
   ///Constructor
   Semester({
-    required this.sem,
+    required this.id,
     required this.title,
     required this.courseID,
     required this.departmentID,
   });
 
   /// No. of the semester (Ex=>1)
-  int sem;
+  int id;
 
   /// Title of the semester (Ex=> 1st Semester)
   String title;
