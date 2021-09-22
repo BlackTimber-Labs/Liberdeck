abc223a2e2cb98d4235386a21577c61e92e17369
Yash Garg <yashygkf@gmail.com>
2021-09-23 01:19:49 +0530

    UI corrections

--- lib/widgets/profile_screen/profile_info.dart
+++ lib/widgets/profile_screen/profile_info.dart
@@ -17,72 +17,72 @@ class _ProfileInfoState extends State<ProfileInfo> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          const Text(
-            'INFORMATION',
-            style: TextStyle(fontSize: 22.5, fontWeight: FontWeight.w600),
-          ),
-          SizedBox(height: 20),
-          Row(
-            // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: const <Widget>[
-               Text(
-                'BRANCH :',
-                style: TextStyle(fontSize: 19.5, fontWeight: FontWeight.w500),
-              ),
-             SizedBox(width: 80),
-              Text(
-                ' CSE',
-                style: TextStyle(
-                    fontSize: 19.5,
-                    fontWeight: FontWeight.w500,
-                    color: Color(0xFFC95A2B)),
-              ),
-            ],
-          ),
-          const SizedBox(height: 20),
-          Row(
-            // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: const <Widget>[
-              Text(
-                'YEAR :',
-                style: TextStyle(fontSize: 19.5, fontWeight: FontWeight.w500),
-              ),
-              SizedBox(width: 110),
-              Text(
-                ' 2nd',
-                style: TextStyle(
-                    fontSize: 19.5,
-                    fontWeight: FontWeight.w500,
-                    color: Color(0xFFC95A2B)),
-              ),
-            ],
-          ),
-          SizedBox(height: 20),
-          Row(
-            // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children:const  <Widget>[
-              Text(
-                'SEMESTER :',
-                style: TextStyle(fontSize: 19.5, fontWeight: FontWeight.w500),
-              ),
-              SizedBox(width: 60),
-              Text(
-                ' 3rd',
-                style: TextStyle(
-                    fontSize: 19.5,
-                    fontWeight: FontWeight.w500,
-                    color: Color(0xFFC95A2B)),
-              ),
-            ],
-          ),
-          SizedBox(height: 30.0),
-          Divider(thickness: 1.0, color: Color(0xFF843622)),
-          SizedBox(height: 30.0),
+          // const Text(
+          //   'INFORMATION',
+          //   style: TextStyle(fontSize: 22.5, fontWeight: FontWeight.w600),
+          // ),
+          // SizedBox(height: 20),
+          // Row(
+          //   // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          //   children: const <Widget>[
+          //      Text(
+          //       'BRANCH :',
+          //       style: TextStyle(fontSize: 19.5, fontWeight: FontWeight.w500),
+          //     ),
+          //    SizedBox(width: 80),
+          //     Text(
+          //       ' CSE',
+          //       style: TextStyle(
+          //           fontSize: 19.5,
+          //           fontWeight: FontWeight.w500,
+          //           color: Color(0xFFC95A2B)),
+          //     ),
+          //   ],
+          // ),
+          // const SizedBox(height: 20),
+          // Row(
+          //   // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          //   children: const <Widget>[
+          //     Text(
+          //       'YEAR :',
+          //       style: TextStyle(fontSize: 19.5, fontWeight: FontWeight.w500),
+          //     ),
+          //     SizedBox(width: 110),
+          //     Text(
+          //       ' 2nd',
+          //       style: TextStyle(
+          //           fontSize: 19.5,
+          //           fontWeight: FontWeight.w500,
+          //           color: Color(0xFFC95A2B)),
+          //     ),
+          //   ],
+          // ),
+          // SizedBox(height: 20),
+          // Row(
+          //   // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          //   children:const  <Widget>[
+          //     Text(
+          //       'SEMESTER :',
+          //       style: TextStyle(fontSize: 19.5, fontWeight: FontWeight.w500),
+          //     ),
+          //     SizedBox(width: 60),
+          //     Text(
+          //       ' 3rd',
+          //       style: TextStyle(
+          //           fontSize: 19.5,
+          //           fontWeight: FontWeight.w500,
+          //           color: Color(0xFFC95A2B)),
+          //     ),
+          //   ],
+          // ),
+          // SizedBox(height: 30.0),
+          // Divider(thickness: 1.0, color: Color(0xFF843622)),
+          // SizedBox(height: 30.0),
           const Text(
             'ACCOUNT',
             style: TextStyle(fontSize: 22.5, fontWeight: FontWeight.w600),
           ),
-          SizedBox(height: 20),
+          const SizedBox(height: 20),
           Row(
             // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
@@ -98,7 +98,7 @@ class _ProfileInfoState extends State<ProfileInfo> {
                     user.displayName!,
                     maxLines: 2,
                     overflow: TextOverflow.ellipsis,
-                    style: TextStyle(
+                    style: const TextStyle(
                         fontSize: 19.5,
                         fontWeight: FontWeight.w500,
                         color: Color(0xFFC95A2B)),
