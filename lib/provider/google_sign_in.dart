bcbdd93e502b5df246366e045579335818f186a9
Yash Garg <yashygkf@gmail.com>
2021-09-23 01:22:33 +0530

    some corrections

--- lib/provider/google_sign_in.dart
+++ lib/provider/google_sign_in.dart
@@ -1,39 +1,57 @@
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:liberdeck/screens/bottom_navigation_screen.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
-class GoogleSignInProvider extends ChangeNotifier{
-  final googleSignIn = GoogleSignIn();
-  GoogleSignInAccount? _user;
-  GoogleSignInAccount get user => _user!;
+import '../screens/bottom_navigation_screen.dart';
+
+class GoogleSignInProvider extends ChangeNotifier {
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+  // GoogleSignInAccount? _user;
+  //GoogleSignInAccount get user => _user!;
 
   Future googleLogIn(BuildContext context) async {
-    try{
-    final googleUser = await googleSignIn.signIn();
-    
-    if (googleUser == null) return;
-    _user = googleUser;
-    final googleAuth = await googleUser.authentication;
+    // try {
+    //   final GoogleSignInAccount? googleUser = await googleSignIn.signIn();
 
-    final credential = GoogleAuthProvider.credential(
-      accessToken: googleAuth.accessToken,
-      idToken: googleAuth.idToken,
-    );
-    await FirebaseAuth.instance.signInWithCredential(credential)
-        .then(
-          (value) =>
-       Navigator.of(context).
-      pushNamed(BottomNavigationScreen.routename),
-    );
-  }
-  catch (e){
-    print(e.toString());
-  }
-    notifyListeners();
+    //   if (googleUser == null) return;
+    //   _user = googleUser;
+    //   final GoogleSignInAuthentication googleAuth =
+    //       await googleUser.authentication;
+
+    //   final OAuthCredential credential = GoogleAuthProvider.credential(
+    //     accessToken: googleAuth.accessToken,
+    //     idToken: googleAuth.idToken,
+    //   );
+    //   await FirebaseAuth.instance.signInWithCredential(credential).then(
+    //         (UserCredential value) => Navigator.of(context)
+    //             .pushNamed(BottomNavigationScreen.routename),
+    //       );
+    // }
+    //final googleSignIn = GoogleSignIn();
+    final GoogleSignInAccount? googleAccount = await googleSignIn.signIn();
+    if (googleAccount != null) {
+      final GoogleSignInAuthentication googleAuth =
+          await googleAccount.authentication;
+      if (googleAuth.accessToken != null && googleAuth.idToken != null) {
+        try {
+          await FirebaseAuth.instance
+              .signInWithCredential(
+                GoogleAuthProvider.credential(
+                  idToken: googleAuth.idToken,
+                  accessToken: googleAuth.accessToken,
+                ),
+              )
+              .then((UserCredential value) => Navigator.of(context)
+                  .pushNamed(BottomNavigationScreen.routename),);
+        } catch (e) {
+          print(e.toString());
+        }
+        notifyListeners();
+      }
+    }
   }
 
-  Future logout() async{
+  Future<void> logout() async {
     await googleSignIn.disconnect();
     FirebaseAuth.instance.signOut();
   }
